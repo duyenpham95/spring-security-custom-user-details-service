ca5ff7296cdef221e61b7fb1200412b4070fc7c6
Duyen <fantasticJilove@gmail.com>
2022-06-08 16:42:06 +0700

    use BCryptPasswordEncoder

--- src/main/resources/scripts/db.sql
+++ src/main/resources/scripts/db.sql
@@ -4,3 +4,6 @@ CREATE TABLE CUSTOM_USER (
 	PWD VARCHAR(100) NOT NULL,
 	ROLE_NAME VARCHAR(20)
 );
+
+-- use https://bcrypt-generator.com/, round 12, password text: 123
+INSERT INTO CUSTOM_USER (id, email, pwd, role_name) VALUES (1, 'duyen@gmail.com', '$2a$12$u3Q8r8XvpQddTHV50rbcKe6VhWm1ItR98uYe2nwF4p80qGLlUG.j6', 'user');
